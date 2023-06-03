d473ad8c152f7c1023519fa2c60a9dbbf5413e39
eslamashraf98 <eslamashraf27@gmail.com>
2023-06-03 23:18:07 +0300

    update

--- elneby-pt.js
+++ elneby-pt.js
@@ -60,3 +60,17 @@ $('.multi-item-carousel .item').each(function(){
   	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
+
+
+
+
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
